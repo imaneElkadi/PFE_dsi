7d68ea8c715f4da2ebaadf2e65ab12684faa1474
imane ELKADI <imaneelkadi22@gmail.com>
2023-04-08 13:52:31 +0000

    add new modif

--- profil.php
+++ profil.php
@@ -1,105 +1,220 @@
+
+
 <!DOCTYPE html>
-<html lang="en" >
+<html lang="en">
 <head>
   <meta charset="UTF-8">
-  <title> The ONTHA </title>
-  <link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Raleway|Spectral'>
-  <link rel="stylesheet" href="myprofil.css">
-
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>The ONTHA</title>
+  <link rel="stylesheet" href="profil.css">
 </head>
 <body>
-<!-- partial:index.partial.html -->
-<div class="burger">
-  <span></span>
-</div>
+  <!-- Header section -->
+  <header>
+  <nav>
+  <ul>
+    <li><a href="#">Home</a></li>
+    <li><a href="#">Shop</a></li>
+    <li><a href="#">Blog</a></li>
+    <li><a href="#">About</a></li>
+    <li><a href="#">Contact</a></li>
+  </ul>
+  <div class="footer__logo">
+      <img src="logoo.png" alt="Logo">
+    </div>
+  <style>
 
-<nav>
-  <ul class="main">
-    <li><a href="index.php">Home</a></li>
-    <li><a href="#0">Shop</a></li>
-    <li><a href="#0">About</a></li>
-    <li><a href="#0">Contact</a></li>
-    <li><a href="#0">Cart <span>(5)</span></a></li>
-  </ul>
-  
-  <div class="about">
-    <p>inner beauty: inner strength, resilience, selflessness, authenticity,
-		 integrity, patience, understanding, and a positive attitude. 
-		 Inner beauty is truly about the goodness that comes from within a person, 
-		and how they share that goodness with the world around them.</p>
-  </div>
-  
-  <div class="social">
-    <a href="https://twitter.com/" target="_blank"><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="64px" height="64px" viewBox="0 0 64 64"><g transform="translate(0, 0)"><path d="M64,12.2c-2.4,1-4.9,1.8-7.5,2.1c2.7-1.6,4.8-4.2,5.8-7.3c-2.5,1.5-5.3,2.6-8.3,3.2C51.5,7.6,48.1,6,44.3,6 c-7.3,0-13.1,5.9-13.1,13.1c0,1,0.1,2,0.3,3C20.6,21.6,10.9,16.3,4.5,8.4c-1.1,1.9-1.8,4.2-1.8,6.6c0,4.6,2.3,8.6,5.8,10.9 c-2.2-0.1-4.2-0.7-5.9-1.6c0,0.1,0,0.1,0,0.2c0,6.4,4.5,11.7,10.5,12.9c-1.1,0.3-2.3,0.5-3.5,0.5c-0.8,0-1.7-0.1-2.5-0.2 c1.7,5.2,6.5,9,12.3,9.1c-4.5,3.5-10.2,5.6-16.3,5.6c-1.1,0-2.1-0.1-3.1-0.2C5.8,55.8,12.7,58,20.1,58c24.2,0,37.4-20,37.4-37.4 c0-0.6,0-1.1,0-1.7C60,17.1,62.2,14.8,64,12.2z"></path></g></svg></a>
-    <a href="https://www.instagram.com/" target="_blank"><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="64px" height="64px" viewBox="0 0 64 64"><g transform="translate(0, 0)"><path d="M32,5.766c8.544,0,9.556,0.033,12.931,0.187c3.642,0.166,7.021,0.895,9.621,3.496 c2.6,2.6,3.329,5.979,3.496,9.621c0.154,3.374,0.187,4.386,0.187,12.931s-0.033,9.556-0.187,12.931 c-0.166,3.642-0.895,7.021-3.496,9.621c-2.6,2.6-5.98,3.329-9.621,3.496c-3.374,0.154-4.386,0.187-12.931,0.187 s-9.557-0.033-12.931-0.187c-3.642-0.166-7.021-0.895-9.621-3.496c-2.6-2.6-3.329-5.979-3.496-9.621 C5.798,41.556,5.766,40.544,5.766,32s0.033-9.556,0.187-12.931c0.166-3.642,0.895-7.021,3.496-9.621 c2.6-2.6,5.979-3.329,9.621-3.496C22.444,5.798,23.456,5.766,32,5.766 M32,0c-8.691,0-9.78,0.037-13.194,0.193 c-5.2,0.237-9.768,1.511-13.436,5.178C1.705,9.037,0.43,13.604,0.193,18.806C0.037,22.22,0,23.309,0,32 c0,8.691,0.037,9.78,0.193,13.194c0.237,5.2,1.511,9.768,5.178,13.436c3.666,3.666,8.234,4.941,13.436,5.178 C22.22,63.963,23.309,64,32,64s9.78-0.037,13.194-0.193c5.199-0.237,9.768-1.511,13.436-5.178c3.666-3.666,4.941-8.234,5.178-13.436 C63.963,41.78,64,40.691,64,32s-0.037-9.78-0.193-13.194c-0.237-5.2-1.511-9.768-5.178-13.436 c-3.666-3.666-8.234-4.941-13.436-5.178C41.78,0.037,40.691,0,32,0L32,0z"></path> <path data-color="color-2" d="M32,15.568c-9.075,0-16.432,7.357-16.432,16.432c0,9.075,7.357,16.432,16.432,16.432 S48.432,41.075,48.432,32C48.432,22.925,41.075,15.568,32,15.568z M32,42.667c-5.891,0-10.667-4.776-10.667-10.667 c0-5.891,4.776-10.667,10.667-10.667c5.891,0,10.667,4.776,10.667,10.667C42.667,37.891,37.891,42.667,32,42.667z"></path> <circle data-color="color-2" cx="49.082" cy="14.918" r="3.84"></circle></g></svg></a>
-  </div>
-  
-  <ul class="sub">
-    <li><a href="#0">FAQ &amp; Shipping</a></li>
-    <li><a href="#0">Terms &amp; Conditions</li>
-  </ul>
+nav {
+  background-color: #f7d3e3; /* set background color */
+  height: 80px; /* set height */
+  display: flex; /* enable flexbox */
+  justify-content: center; /* center items horizontally */
+  align-items: center; /* center items vertically */
+  border-bottom: 2px solid #e6a9c6; /* add bottom border */
+  box-shadow: 0px 3px 10px rgba(0, 0, 0, 0.2); /* add box shadow */
+ 
+}
+
+ul {
+  list-style: none; /* remove bullet points */
+  display: flex; /* enable flexbox */
+  margin: 0;
+  padding: 0;
+}
+
+li {
+  margin: 0 15px; /* add margin between items */
+}
+
+a {
+  color: #333; /* set link color */
+  text-decoration: none; /* remove underline */
+  font-size: 20px; /* set font size */
+  font-weight: bold; /* set font weight */
+  transition: all 0.3s ease; /* add transition effect */
+  position: relative; /* enable absolute positioning */
+}
+
+a:before {
+  content: ""; /* add empty content */
+  position: absolute; /* set position to absolute */
+  bottom: -5px; /* position element below text */
+  left: 0; /* align to left */
+  width: 100%; /* set width to 100% */
+  height: 3px; /* set height */
+  background-color: #e6a9c6; /* set background color */
+  visibility: hidden; /* hide element by default */
+  transform: scaleX(0); /* scale element horizontally */
+  transition: all 0.3s ease; /* add transition effect */
+}
+
+a:hover:before {
+  visibility: visible; /* show element on hover */
+  transform: scaleX(1); /* scale element horizontally */
+}
+
+a:hover {
+  color: #ff69b4; /* change link color on hover */
+}
+
+  </style>
 </nav>
-<div class="overlay"></div>
-<!-- partial -->
-  <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js'>
-
-  </script><script  src="myprofil.js"></script>
 
 
-
-<div class="profile">
-  <div class="profile-header">
+    <div class="header__content">
+      <h1><p style=" padding-top: 50px;" class="cursor typewriter-animation">While physical features can
+         be pleasing, a woman's inner beauty, strength, 
+        and resilience are what truly make her beautiful 🖤 .
     
-    <div class="profile-details">
-      <h1>Jane Doe</h1>
-      <br>
-      <h2>Software Engineer</h2>
-      <br><br>
-      <ul class="cute">
-        <li><i class="fas fa-map-marker-alt"></i> Marrakech bouakaz  num 41 zerktouni neare menara</li>
-        <li><i class="fas fa-birthday-cake"></i>6 july 2002</li>
-        <li><i class="fas fa-envelope"></i> imaneelkadi22@gmail.com</li>
-        <li><i class="fas fa-phone"></i> 0708867960</li>
-        <li><i class="fas fa-link"></i> <a href="https://www.example.com">www.example.com</a></li>
-      </ul>
+        <style>
+
+          /* Google Fonts */
+@import url(https://fonts.googleapis.com/css?family=Anonymous+Pro);
+
+/* Cursor */
+.cursor{
+    position: relative;
+    width: 24em;
+    margin: 0 auto;
+    border-right: 1px solid rgba(255,255,255,.75);
+    font-size: 130px;
+    text-align: center;
+    white-space: nowrap;
+    overflow: hidden;
+    transform: translateY(-50%); 
+     
+}
+/* Animation */
+.typewriter-animation {
+  animation: 
+    typewriter 5s steps(50) 1s 1 normal both, 
+    blinkingCursor 500ms steps(50) infinite normal;
+}
+@keyframes typewriter {
+  from { width: 0; }
+  to { width: 100%; }
+}
+@keyframes blinkingCursor{
+  from { border-right-color: rgba(255,255,255,.75); }
+  to { border-right-color: transparent; }
+}
+
+
+
+
+
+
+        </style>
+        <script>
+
+        </script>
+      </p></h1>
+      
+     
     </div>
-  </div>
-  <div class="profile-bio">
-    <h3>Write your self</h3>
-  
-<textarea rows="4" cols="50"  id="message" name="message" placeholder="Enter your message here" ></textarea>
+  </header>
+
+
+
 
   </div>
-  <div class="profile-photos">
-    <h3>Photos</h3>
-    <div class="photo-grid">
-      <img src="https://via.placeholder.com/300x300.png?text=Photo+1" alt="Photo 1">
-      <img src="https://via.placeholder.com/300x300.png?text=Photo+2" alt="Photo 2">
-      <img src="https://via.placeholder.com/300x300.png?text=Photo+3" alt="Photo 3">
-      <img src="https://via.placeholder.com/300x300.png?text=Photo+4" alt="Photo 4">
-      <img src="https://via.placeholder.com/300x300.png?text=Photo+5" alt="Photo 5">
-      <img src="https://via.placeholder.com/300x300.png?text=Photo+6" alt="Photo 6">
-    </div>
+
+  <!-- About section -->
+  <section class="about">
+   
+   
+  </section>
+
+  <!-- Blog section -->
+  <section class="blog">
+  
+<div  style="  display: flex;">
+
+
+      <div class="blog__post" >
+        
+      <div class="cadre" >
+  <h2>Ovulation and Period Calculator</h2>
+  <p> Amazing things  what woman's body is capable of.</p>
+  <a href="regle.php">Click here to learn more</a>
+</div>
+<div class="shopping-guide">
+  <h2>Shop for your Fashion</h2>
+  <!-- <p style="color:black;">Find the latest fashion trends and styles for women at our online store.</p> -->
+  <a href="shop.php" class="cta-button">Shop Now</a>
+</div>
   </div>
+
+  
 </div>
 
+</div>
+</section>
+  <!-- Contact section -->
+  <section class="get-in-touch">
+  <div class="container">
+  <div class="footer__logo">
+      <img src="logoo.png" alt="Logo">
+</div>
+    
+    <h2 class="get-in-touch__heading">Get in touch</h2>
+    <center>
+    <p style="      font-family: 'Montserrat', sans-serif;
+    font-size: 24px;
+    line-height: 1.5;
+    color: #333;
+    text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.3);">Got a question or feedback? Contact us!</p></center>
+    <form class="get-in-touch__form" action="#" method="post">
+      <div class="form-group">
+        <input class="form-control" type="text" placeholder="Name" required>
+      </div>
+      <div class="form-group">
+        <input class="form-control" type="email" placeholder="Email" required>
+      </div>
 
+      <div class="form-group">
+        <textarea class="form-control" placeholder="Message" required></textarea>
+      </div>
+      <div>
+<center>
+ 
+<button class="btn2"> Send</button>
 
+      </center>
+      </div>
+    </form>
+  </div>
+</section>
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  <!-- Footer section -->
+  <footer>
+    <div class="footer__logo">
+      <img src="logoo.png" alt="Logo">
+    </div>
+    
+    <div class="footer__info">
+      <p style="color:black;"> © 2023 The ONTHA</p>
+    </div>
+  </footer>
 </body>
 </html>
