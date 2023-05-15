a0862642511e40c9e228d554fbd0a6be759e4aeb
imane ELKADI <imaneelkadi22@gmail.com>
2023-05-16 00:44:01 +0100

    new modif in shop

---
+++ dress.php
@@ -0,0 +1,126 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>The Ontha</title>
+  <link rel="stylesheet" href="dress.css">
+</head>
+<body>
+  
+
+  <main>
+    <section class="banner">
+      <h1>Welcome to Your Store for dresses</h1>
+      <p style="color:grey; font-size:30px;">Discover amazing deals and discounts!</p>
+      <div class="logo">
+  <img src="shop/dress.png" alt="Logo">
+  
+</div>
+<div class="logo2">
+  <img src="shop/dress.png" alt="Logo">
+  
+</div>
+    </section>
+
+    <section class="featured-products">
+     
+      <div class="product">
+        <img src="shop/dress1.png" alt="Product 1">
+        <h3>60.00 $</h3>
+        <p>Summer dress square neck.</p>
+        <button>Add to Cart</button>
+      </div>
+      <div class="product">
+        <img src="shop/dress2.jpg" alt="Product 2">
+        <h3>56.00 $</h3>
+        <p>Liyge shoulder dress.</p>
+        <button>Add to Cart</button>
+      </div>
+      <div class="product">
+        <img src="shop/dress3.jpg" alt="Product 3">
+        <h3>70.00 $</h3>
+        <p>Long green dress for summer.</p>
+        <button>Add to Cart</button>
+      </div>
+      <div class="product">
+        <img src="shop/dress4.jpg" alt="Product 4">
+        <h3>74.00 $</h3>
+        <p>Yellow selkie puff dress dupe.</p>
+        <button>Add to Cart</button>
+      </div>
+      <div class="product">
+        <img src="shop/dress5.jpg" alt="Product 5">
+        <h3>59.00 $</h3>
+        <p>Elegant midi length dress.</p>
+        <button>Add to Cart</button>
+      </div>
+      <div class="product">
+        <img src="shop/dress6.jpg" alt="Product 5">
+        <h3>80.00 $</h3>
+        <p>Decent & Stylish hijab dress & Abaya</p>
+        <button>Add to Cart</button>
+      </div>
+      <div class="product">
+        <img src="shop/dress7.jpg" alt="Product 5">
+        <h3>61.00 $</h3>
+        <p>blue decent Abaya.</p>
+        <button>Add to Cart</button>
+      </div>
+      <div class="product">
+        <img src="shop/dress8.jpg" alt="Product 5">
+        <h3>90.00 $</h3>
+        <p>Ramadan out fit</p>
+        <button>Add to Cart</button>
+      </div>
+      <div class="product">
+        <img src="shop/dress9.jpg" alt="Product 5">
+        <h3>55.00 $</h3>
+        <p>Iftar party outfit.</p>
+        <button>Add to Cart</button>
+      </div>
+      <div class="product">
+        <img src="shop/dress10.jpg" alt="Product 5">
+        <h3>41.00 $</h3>
+        <p>Rigular fit elegant dress.</p>
+        <button>Add to Cart</button>
+      </div>
+      <div class="product">
+        <img src="shop/dress10.jpg" alt="Product 5">
+        <h3>41.00 $</h3>
+        <p>Rigular fit elegant dress.</p>
+        <button>Add to Cart</button>
+      </div>
+      <div class="product">
+        <img src="shop/dress10.jpg" alt="Product 5">
+        <h3>41.00 $</h3>
+        <p>Rigular fit elegant dress.</p>
+        <button>Add to Cart</button>
+      </div>
+      <div class="product">
+        <img src="shop/dress10.jpg" alt="Product 5">
+        <h3>41.00 $</h3>
+        <p>Rigular fit elegant dress.</p>
+        <button>Add to Cart</button>
+      </div>
+      <div class="product">
+        <img src="shop/dress10.jpg" alt="Product 5">
+        <h3>41.00 $</h3>
+        <p>Rigular fit elegant dress.</p>
+        <button>Add to Cart</button>
+      </div>
+      <div class="product">
+        <img src="shop/dress10.jpg" alt="Product 5">
+        <h3>41.00 $</h3>
+        <p>Rigular fit elegant dress.</p>
+        <button>Add to Cart</button>
+      </div>
+      <!-- Add more product items here -->
+    </section>
+  </main>
+
+  <footer>
+    <p>&copy; 2023 The Ontha.</p>
+  </footer>
+</body>
+</html>
--- shop.php
+++ shop.php
@@ -1,124 +1,198 @@
 <!DOCTYPE html>
 <html>
 <head>
-  <title>The Ontha</title>
+  <title>Shopping Page</title>
+  <link rel="stylesheet" href="shop.css">
   <style>
-    body {
+    /* Fancy CSS styles */
+      body {
       font-family: Arial, sans-serif;
+      background-color: #f1f1f1;
       margin: 0;
       padding: 0;
     }
 
-    header {
-      background-color:#D2B4DE;
-      color: #fff;
-      padding: 20px;
-      text-align: center;
-    }
-
-    h1 {
-      margin: 0;
-    }
-
     .container {
-      max-width: 900px;
-      margin: 20px auto;
+      max-width: 1200px;
+      margin: 0 auto;
+      padding: 20px;
+    }
+
+    .header {
+      text-align: center;
+      padding: 20px;
+      background-color: #fcc8f8;
+      color: black;
+    }
+
+    .options {
+      text-align: center;
+      padding: 20px;
+    }
+
+    .options select {
+      padding: 10px;
+      font-size: 16px;
+      border-radius: 4px;
+    }
+
+    .products {
+      display: flex;
+      flex-wrap: wrap;
+      justify-content: center;
     }
 
     .product {
-      display: flex;
-      align-items: center;
-      margin-bottom: 20px;
+      width: 300px;
+      margin: 20px;
       padding: 20px;
-      background-color: #f2f2f2;
-      border-radius: 5px;
-      box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+      background-color: #fff;
+      border-radius: 4px;
+      box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
     }
 
-    .product-image {
-      flex-basis: 30%;
-      margin-right: 20px;
-    }
-
-    .product-image img {
-      max-width: 100%;
+    .product img {
+      width: 100%;
       height: auto;
-    }
-
-    .product-details {
-      flex-basis: 70%;
-    }
-
-    .product-title {
-      font-weight: bold;
       margin-bottom: 10px;
     }
 
-    .product-price {
+    .product h3 {
+      margin: 0;
+      font-size: 18px;
+    }
+
+    .product p {
       color: #888;
-      margin-bottom: 10px;
+      font-size: 14px;
     }
+    .logo {
+  width: 60px; /* Adjust the width as needed */
+  float: left; /* Positions the logo on the left */
+}
 
-    .product-description {
-      margin-bottom: 10px;
-    }
 
-    .button {
-      padding: 10px 20px;
-      background-color: #333;
-      color: #fff;
-      border: none;
-      border-radius: 3px;
-      cursor: pointer;
-    }
+.logo img {
+  width: 100%;
+  height: auto;
 
-    .button:hover {
-      background-color: #555;
-    }
+}
+.logo2 {
+  width: 60px; /* Adjust the width as needed */
+  float: right; /* Positions the logo on the left */
+}
+
+
+.logo2 img {
+  width: 100%;
+  height: auto;
+
+}
+button {
+ padding: 1rem 2rem;
+ font-weight: 700;
+ background: rgb(255, 255, 255);
+ color: blueviolet;
+ border-radius: .5rem;
+ border-bottom: 2px solid blueviolet;
+ border-right: 2px solid blueviolet;
+ border-top: 2px solid white;
+ border-left: 2px solid white;
+ transition-duration: 1s;
+ transition-property: border-top, 
+    border-left, 
+    border-bottom,
+    border-right,
+    box-shadow;
+}
+
+button:hover {
+ border-top: 2px solid blueviolet;
+ border-left: 2px solid blueviolet;
+ border-bottom: 2px solid rgb(238, 103, 238);
+ border-right: 2px solid rgb(238, 103, 238);
+ box-shadow: rgba(240, 46, 170, 0.4) 5px 5px, rgba(240, 46, 170, 0.3) 10px 10px, rgba(240, 46, 170, 0.2) 15px 15px;
+}
   </style>
 </head>
 <body>
-  <header>
-    <h1>The Ontha shopping</h1>
-  </header>
-
   <div class="container">
-    <div class="product">
-      <div class="product-image">
-        <img src="product1.jpg" alt="Product 1">
-      </div>
-      <div class="product-details">
-        <h2 class="product-title">Product 1</h2>
-        <p class="product-price">$19.99</p>
-        <p class="product-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce tempus tincidunt risus, vel facilisis nulla congue et.</p>
-        <button class="button">Add to Cart</button>
-      </div>
+    <div class="header">
+      <h1>The Ontha Shopping </h1>
+      <div class="logo">
+  <img src="logoo.png" alt="Logo">
+  
+</div>
+<div class="logo2">
+  <img src="logoo.png" alt="Logo">
+  
+</div>
     </div>
 
-    <div class="product">
-      <div class="product-image">
-        <img src="product2.jpg" alt="Product 2">
-      </div>
-      <div class="product-details">
-        <h2 class="product-title">Product 2</h2>
-        <p class="product-price">$24.99</p>
-        <p class="product-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce tempus tincidunt risus, vel facilisis nulla congue et.</p>
-        <button class="button">Add to Cart</button>
-      </div>
+    <div class="options">
+      <label for="gender">Choose Categorie:</label>
+      <select id="gender" onchange="location.href=this.value;">
+        <option value="dress.php">Dresses</option>
+        <option value="pant.php">Pants</option>
+        <option value="top.php">Tops</option>
+        <option value="jacket.php">Jackets</option>
+        <option value="shoes.php">Shoes</option>
+      </select>
     </div>
 
-    <div class="product">
-      <div class="product-image">
-        <img src="product3.jpg" alt="Product 3">
+    <div class="products">
+      <!-- Product 1 -->
+      <div class="product">
+        <img src="shop/item1.png" alt="Product 1">
+        <h3> 61.50 $</h3>
+     
+       <a  style="text-decoration:none;" href="https://ma.shein.com/SHEIN-MOD-Ditsy-Floral-Print-Puff-Sleeve-Knot-Front-Split-Thigh-Dress-p-15331173-cat-1727.html?src_identifier=on%3DIMAGE_COMPONENT%60cn%3DIMAGE_COMPONENT_2%60hz%3DhotZone_1%60ps%3D2_1%60jc%3Dreal_1727&src_module=Women&src_tab_page_id=page_home1684184351137&mallCode=1"> View Product</a>
+       
+       <button>Add to Cart</button>
       </div>
-      <div class="product-details">
-        <h2 class="product-title">Product 3</h2>
-        <p class="product-price">$14.99</p>
-        <p class="product-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce tempus tincidunt risus, vel facilisis nulla congue et.</p>
-        <button class="button">Add to Cart</button>
+
+      <!-- Product 2 -->
+      <div class="product">
+        <img src="shop/item2.png" alt="Product 2">
+        <h3>113.0 $</h3>
+        <a style="text-decoration:none;" href="https://ma.shein.com/Women-Bow-Decor-Point-Toe-Chunky-Heeled-Pumps-Funky-Red-Lycra-Ankle-Strap-Pumps-p-15589821-cat-1750.html?src_identifier=on%3DIMAGE_COMPONENT%60cn%3DIMAGE_COMPONENT_2%60hz%3DhotZone_8%60ps%3D2_8%60jc%3Dreal_1745&src_module=Women&src_tab_page_id=page_home1684186766144&mallCode=1">View Product</a>
+        <button>Add to Cart</button>
+      </div>
+
+      <!-- Product 3 -->
+      <div class="product">
+        <img src="shop/item3.png" alt="Product 3">
+        <h3>69.70 $</h3>
+       <a style="text-decoration:none;" href="https://ma.shein.com/SHEIN-Unity-E-Women-Coats-p-15493028-cat-1735.html?src_identifier=on%3DIMAGE_COMPONENT%60cn%3DIMAGE_COMPONENT_2%60hz%3DhotZone_3%60ps%3D2_3%60jc%3Dreal_2037&src_module=Women&src_tab_page_id=page_home1684186807013&mallCode=1">View Product</a>
+        <button>Add to Cart</button>
+      </div>
+         <!-- Product 4 -->
+         <div class="product">
+        <img src="shop/item4.png" alt="Product 3">
+        <h3>70.80 $</h3>
+       <a style="text-decoration:none;" href="https://ma.shein.com/Contrast-Plaid-Button-Front-Overshirt-p-15074578-cat-1735.html?src_identifier=on%3DIMAGE_COMPONENT%60cn%3DIMAGE_COMPONENT_2%60hz%3DhotZone_3%60ps%3D2_3%60jc%3Dreal_2037&src_module=Women&src_tab_page_id=page_home1684186807013&mallCode=1">View Product</a>
+        <button>Add to Cart</button>
+      </div>
+         <!-- Product 5 -->
+         <div class="product">
+        <img src="shop/item5.png" alt="Product 3">
+        <h3>104.10 $</h3>
+       <a style="text-decoration:none;" href="https://ma.shein.com/SHEIN-PETITE-High-Waist-Straight-Leg-Jeans-p-15420271-cat-1934.html?src_identifier=on%3DIMAGE_COMPONENT%60cn%3DIMAGE_COMPONENT_2%60hz%3DhotZone_7%60ps%3D2_7%60jc%3Dreal_1930&src_module=Women&src_tab_page_id=page_home1684189468346&mallCode=1">View Product</a>
+        <button>Add to Cart</button>
+      </div>
+         <!-- Product 6 -->
+         <div class="product">
+        <img src="shop/item6.png" alt="Product 3">
+        <h3>22.20 $</h3>
+       <a style="text-decoration:none;" href="https://ma.shein.com/SHEIN-EZwear-Split-Hem-Tube-Top-p-15633259-cat-2223.html?src_identifier=on%3DIMAGE_COMPONENT%60cn%3DIMAGE_COMPONENT_2%60hz%3DhotZone_2%60ps%3D2_2%60jc%3Dreal_1766&src_module=Women&src_tab_page_id=page_home1684189692538&mallCode=1">View Product</a>
+        <button>Add to Cart</button>
       </div>
     </div>
   </div>
+  <footer>
+  <div class="footer-container">
+    <p>&copy; 2023 The Ontha</p>
+  </div>
+</footer>
 </body>
 </html>
-
