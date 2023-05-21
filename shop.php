b065d8730712d419e3910005c83cb5e7dd5e9966
imane ELKADI <imaneelkadi22@gmail.com>
2023-05-21 22:18:01 +0100

    ,

--- shop.php
+++ shop.php
@@ -3,8 +3,8 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Créer un site e-commerce</title>
-  <link rel="stylesheet" href="shoop.css">
+  <title>The Ontha</title>
+  <link rel="stylesheet" href="shop.css">
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.13.0/css/all.css" />
 </head>
 <body>
@@ -24,21 +24,30 @@
       <form>
         <input type="search" placeholder="Rechercher">
       </form>
-      
-      <p><i class="far fa-heart"></i></p>
-      <p><i class="fas fa-shopping-cart"></i></p>
+      <a href="profil.php">
+      <p><i class="far fa-heart"></i></p></a>
+      <a href="paye.php">
+      <p><i class="fas fa-shopping-cart"></i></p></a>
     </div>
   </nav>
   <!-- Fin de la barre de navigation -->
-  
-  <!-- Header -->
+  <div class="containerImg">
+        <img src="shop/img.png" alt="Image">
+    </div>
+      <!-- Header -->
    <header>
      <h1>This is your shop, your home.</h1>
      <a href="contact.php">
-  <button>Contact us <i class="fas fa-paper-plane"></i></button>
+  <button style="background-color:#b67ba4;">Contact us <i class="fas fa-paper-plane"></i></button>
 </a>
    </header>
   <!-- Fin du header -->
+    <div class="containerImg3">
+        <img src="shop/shop3.jpg" alt="Image">
+    </div>
+ 
+    
+
   
   <!-- Section principale -->
   <section class="main">
@@ -48,35 +57,35 @@
     <div class="cards">
       
       <div class="card">
-        <img src="https://static.nike.com/a/images/f_auto/q_auto:eco/t_PDP_864_v1/0697fd03-09fd-4b3b-be0b-11b009c671d9/short-en-molleton-sportswear-pour-z5Kgjx.jpg">
+        <img src="shop/c3.jpg">
         <div class="card-header">
-          <h4 class="title">Short Nike</h4>
-          <h4 class="price">199$</h4>
+          <h4 class="title">Sweather</h4>
+          <h4 class="price">25.00$</h4>
         </div>
         <div class="card-body">
-          <p>Short moulant pour homme</p>
+          <p>Embrace warmth and comfort in style.</p>
         </div>
       </div>
       
       <div class="card">
-        <img src="https://static.nike.com/a/images/f_auto/q_auto:eco/t_PDP_864_v1/eric5lwitzffpoisq0rj/chaussure-blazer-mid-77-vintage-pour-pMfjs8.jpg">
+        <img src="shop/c4.jpg">
         <div class="card-header">
-          <h4 class="title">Paire de chaussures stylées</h4>
-          <h4 class="price">69$</h4>
+          <h4 class="title">White short dress</h4>
+          <h4 class="price">48.00$</h4>
         </div>
         <div class="card-body">
-          <p>Des paires de chaussures stylées pour femme</p>
+          <p>Effortless beauty for any occasion.</p>
         </div>
       </div>
       
       <div class="card">
-        <img src="https://static.nike.com/a/images/f_auto/q_auto:eco/t_PDP_864_v1/abb537eb-0e70-4e1f-903e-ec46b8657c0d/short-de-training-en-tissu-fleece-dri-fit-pour-n97b2F.jpg">
+        <img src="shop/c1.jpg">
         <div class="card-header">
-          <h4 class="title">Short pour courir</h4>
-          <h4 class="price">39$</h4>
+          <h4 class="title">Leather jacket</h4>
+          <h4 class="price">55.00$</h4>
         </div>
         <div class="card-body">
-          <p>Sport pour courir en plain air pour homme</p>
+          <p>Timeless style meets rugged sophistication.</p>
         </div>
       </div>
       
@@ -84,15 +93,22 @@
     <!-- Fin de toutes les cartes -->
     
     <!-- Video de presentation -->
-    <div class="video">
-      <iframe src="https://www.youtube.com/embed/2COSkxxOtXY" allowfullscreen></iframe>
-    </div>
+    <br>
+    <br>
+    
+   
+    
     <!-- Fin de la video de presentation -->
   </section>
   <!-- Fin de la section principale -->
-  
+  <div class="video-container">
+    <video src="shop/ved1.mp4" autoplay muted></video>
+  </div>
+
+   
+
   <!-- Pied de page -->
-  <footer>
+  <!-- <footer>
     <p>&copy; Contactez-nous au 06 98 87 82 00</p>
     <div class="social-media">
       <p><i class="fab fa-facebook-f"></i></p>
@@ -100,7 +116,44 @@
       <p><i class="fab fa-instagram"></i></p>
       <p><i class="fab fa-linkedin-in"></i></p>
     </div>
-  </footer>
+  </footer> -->
   <!-- Fin du pied de page -->
+
+  <footer>
+  <div class="footer-container">
+    <div class="footer-section">
+      <h3>About Us</h3>
+      <p>Welcome to our beauty website! <br>We are dedicated to providing you <br> 
+        with the latest beauty trends,<br>  tips, and products.  <br>Explore our wide range of skincare, <br> makeup, haircare, and fragrance options  <br>to enhance your natural  <br>beauty and feel confident every day.</p>
+    </div>
+    <div class="footer-section">
+      <h3>Contact</h3>
+      <p>Phone: 07 08 86 79 60</p>
+      <p>Email: imaneelkadi22@.com</p>
+      <p>Address: Marrakech, geliz</p>
+    </div>
+    <div class="footer-section">
+      <h3>Categories</h3>
+      <ul>
+        <li><a href="#">beauty</a></li>
+        <li><a href="#">skin care</a></li>
+        <li><a href="#">make up &amp; hair</a></li>
+        <li><a href="#">clothes &amp; shopping</a></li>
+      </ul>
+    </div>
+    <div class="footer-section">
+      <h3>Follow Us</h3>
+      <ul class="social-media">
+        <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+        <li><a href="#"><i class="fab fa-twitter"></i></a></li>
+        <li><a href="#"><i class="fab fa-instagram"></i></a></li>
+        <li><a href="#"><i class="fab fa-pinterest"></i></a></li>
+      </ul>
+    </div>
+  </div>
+  <div class="footer-bottom">
+    <p>&copy; 2023 The Ontha. All rights reserved.</p>
+  </div>
+</footer>
 </body>
 </html>
